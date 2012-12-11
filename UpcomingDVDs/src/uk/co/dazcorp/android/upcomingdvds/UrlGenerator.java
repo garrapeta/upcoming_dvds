bd647c352f6c4a62a41c0e1ead3ac45d020fb1b1
Daryl Gent <daryl.gent@accenture.com>
2012-12-11 23:48:53 +0000

    Partial API Details filled in
    the api details need some more work on them still.

---
+++ UpcomingDVDs/src/uk/co/dazcorp/android/upcomingdvds/UrlGenerator.java
@@ -0,0 +1,57 @@
+
+package uk.co.dazcorp.android.upcomingdvds;
+
+import android.util.Log;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+class urlGenerator {
+    private static String TAG = "UrlGenerator";
+    private static String API_KEY = "apikey";
+    private static String mApiKey = "bye7be3mwyn6jwphp626jrf4";
+    public static String UPCOMING_DVD_API = "http://api.rottentomatoes.com/api/public/v1.0/lists/dvds/upcoming.json?";
+
+    private String mCurrentURL;
+
+    public urlGenerator(String url, boolean isBase) {
+        if (isBase) {
+            setBaseUrlTo(url);
+        } else {
+            setCurrentURL(url);
+        }
+    }
+
+    public void setBaseUrlTo(String baseUrl) {
+        mCurrentURL = baseUrl;
+        mCurrentURL = mCurrentURL.concat(API_KEY + "=" + mApiKey);
+    }
+
+    public void setCurrentURL(String current) {
+        mCurrentURL = current;
+    }
+
+    public String getCurrentURL() {
+        return mCurrentURL;
+    }
+
+    public boolean addValue(String value_type, String value) {
+        if (value == null || value.equals("")) {
+            return false;
+        }
+
+        Log.d(TAG, "URL - value type: " + value_type);
+        value = value.trim();
+
+        try {
+            value = URLEncoder.encode(value, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            // TODO Should probably do something here
+            e.printStackTrace();
+        }
+        mCurrentURL = mCurrentURL.concat("&" + value_type + "=" + value);
+
+        return true;
+    }
+
+ }
